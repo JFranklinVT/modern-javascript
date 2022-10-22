f04e9528969e35483cc7417dd53ff5ba59897fc7
JFranklinVT <111754521+JFranklinVT@users.noreply.github.com>
2022-10-22 14:30:08 -0400

    updated with notes

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,12 +1,19 @@
 // const para = document.querySelector('p');
 // const para = document.querySelector('.error');
-const para = document.querySelector('div.error');
+// const para = document.querySelector('div.error');
 
-console.log(para);
+// console.log(para);
 
 // query multiple elements at once
 const paras = document.querySelectorAll('p');
 const errors = document.querySelectorAll('.error');
 
-console.log(paras, errors);
-console.log(paras[1], errors[0]);
+// console.log(paras, errors);
+// console.log(paras[1], errors[0]);
+
+console.log(errors);
+// console.log(errors[0]);
+
+// paras.forEach(para => {
+//     console.log(para);
+// })
