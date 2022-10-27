7c03fe7024300e14ee50a445aefa36664e20182e
JFranklinVT <111754521+JFranklinVT@users.noreply.github.com>
2022-10-27 09:57:10 -0400

    finished first to do list on my own

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -2,7 +2,10 @@ const form = document.querySelector('.signup-form');
 const tdList = document.querySelector('.tdList');
 
 tdList.addEventListener('click', event => {
-  
+  console.log(event)
+  if(event.target.tagName === 'LI'){
+    event.target.remove();
+  }
 });
 
 form.addEventListener('submit', e => {
@@ -10,4 +13,8 @@ form.addEventListener('submit', e => {
   // console.log('form submitted');
   // console.log(username.value);
   console.log(form.username.value);
+  const listItem = document.createElement('li');
+  listItem.textContent = form.username.value;
+  tdList.appendChild(listItem);
+  form.username.value = '';
 });
